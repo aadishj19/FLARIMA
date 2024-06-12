cd6eb83a6378970b6f1f77edbd6c0a244f28defc
aadishj19 <84670503+aadishj19@users.noreply.github.com>
2024-06-12 14:29:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from data_processing import analyze_tess_data_from_directory
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Define file paths (update these as needed)
-directory = r'C:\Users\aadis\Downloads\comparison'
-trf_file = r'C:\Users\aadis\Downloads\tess-response-function-v2.0.csv'
-pecaut_mamajek_file = r'C:\Users\aadis\Downloads\PecautMamajek2013.txt'
+directory = r'C:\Users\path_to_directory'
+trf_file = r'C:\Users\path_to_directory\tess-response-function-v2.0.csv'
+pecaut_mamajek_file = r'C:\Users\path_to_directory\PecautMamajek2013.txt'
 
 if __name__ == "__main__":
     analyze_tess_data_from_directory(directory, trf_file, pecaut_mamajek_file)
